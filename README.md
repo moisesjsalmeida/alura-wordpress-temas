d3afd06c33ee77cefa059ace3702584900b69996
Moises Almeida <moisesjsalmeida@gmail.com>
2021-08-04 16:09:18 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# alura-wordpress-temas
---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <p class="container-alura">
+        &copy; <?= date('Y') ?> - Todos os direitos reservados a Alura Intercambios
+    </p>
+</footer>
+</body>
+<?php wp_footer(); ?>
+
+</html>
